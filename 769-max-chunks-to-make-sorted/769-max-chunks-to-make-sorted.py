31e6e5382b79877e29605118b51fe05207825af1
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-02-22 15:59:07 +0530

    Time: 45 ms (37.57%), Space: 13.9 MB (73.31%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.py
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         n = len(arr)-1
-        i = j = 0
+        j = 0
         x = 1
         m = arr[0]
         while j < n:
@@ -9,6 +9,5 @@ class Solution:
                 m = arr[j]
             if arr[j] < arr[j+1] and j == m:
                 x += 1
-                i = j+1
             j += 1
         return x
