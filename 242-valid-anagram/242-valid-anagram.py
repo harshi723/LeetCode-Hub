a717554bceb87e7e66b27add2e67ceba6d2f510f
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-03-01 14:38:34 +0530

    Time: 5187 ms (5.06%), Space: 14.5 MB (75.57%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -3,11 +3,7 @@ class Solution:
         
         if len(s) != len(t) or len(set(s)) != len(set(t)):
             return False
-        l = {}
         for i in s:
-            l[i] = 1+l.get(i,0)
-        for i in t:
-            if i not in l or l[i] == 0:
+            if s.count(i) != t.count(i):
                 return False
-            l[i] = l[i] - 1
         return True
