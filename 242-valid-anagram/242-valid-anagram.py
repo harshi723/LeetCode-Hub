992d3c7924caa558e39568ff43ec00f84e3765bc
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-03-01 14:37:29 +0530

    Time: 56 ms (74.06%), Space: 14.5 MB (75.57%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        if len(s) != len(t):
+        if len(s) != len(t) or len(set(s)) != len(set(t)):
             return False
         l = {}
         for i in s:
