39d26e9624b330794d754caf3604391e0a133ced
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-03-01 14:36:03 +0530

    Time: 56 ms (74.06%), Space: 14.5 MB (75.57%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        l = {}
+        for i in s:
+            l[i] = 1+l.get(i,0)
+        for i in t:
+            if i not in l or l[i] == 0:
+                return False
+            l[i] = l[i] - 1
+        return True
