036e37d6a7715c3f8d8c468d8dfa66f1bfc73a3c
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-06-30 22:39:18 +0530

    Time: 1961 ms (15.84%), Space: 25.1 MB (37.78%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -2,9 +2,9 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         minp = prices[0]
         maxprofit = 0
-        for i in range(len(prices)):
-            currp = prices[i]
-            if minp > currp: 
+        for i in prices:
+            currp = i
+            if minp > currp:
                 minp = currp
             elif maxprofit < currp - minp:
                 maxprofit = currp - minp
