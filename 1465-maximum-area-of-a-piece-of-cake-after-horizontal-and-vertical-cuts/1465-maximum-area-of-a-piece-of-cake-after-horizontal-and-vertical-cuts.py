df1e0d447bbe65e742efcadb071e8326b90af4c9
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-03 02:12:58 +0530

    Time: 372 ms (76.14%), Space: 26.7 MB (94.03%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,25 +1,15 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        horizontalCuts.append(0)
+        horizontalCuts.append(h)
+        verticalCuts.append(0)
+        verticalCuts.append(w)
         horizontalCuts.sort()
-        verticalCuts.sort()
-        x = []
-        y = []
-        for i in range(len(horizontalCuts)):
-            if i == 0:
-                x.append(horizontalCuts[i])
-            if i != 0:
-                x.append(horizontalCuts[i]-horizontalCuts[i-1])
-            if i == len(horizontalCuts)-1:
-                x.append(h - horizontalCuts[i])
-                
-        for j in range(len(verticalCuts)):
-            if j == 0:
-                y.append(verticalCuts[j])
-            if j == len(verticalCuts)-1:
-                y.append(w - verticalCuts[j])
-            if j != 0:
-                y.append(verticalCuts[j]-verticalCuts[j-1])
-            
-        return (max(x)*max(y))%(10**9 + 7)
-        
-        
+        verticalCuts.sort()   
+        l = b = 0
+
+        for i in range(1, len(horizontalCuts)):
+            b = max(b, horizontalCuts[i]-horizontalCuts[i-1])
+        for i in range(1, len(verticalCuts)):
+            l = max(l, verticalCuts[i]-verticalCuts[i-1])
+        return (l*b)%(10**9 + 7)
