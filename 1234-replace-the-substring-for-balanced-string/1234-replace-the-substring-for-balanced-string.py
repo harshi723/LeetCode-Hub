7a9b3a9775652bb3566b9cddf021e64fa86f21e9
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-04 03:13:53 +0530

    Time: 302 ms (67.68%), Space: 14.8 MB (39.92%) - LeetHub

---
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        x = collections.Counter(s)
+        extras = {}
+        for i in x:
+            if x[i] > len(s)//4:
+                extras[i] = x[i]-len(s)//4
+        if not extras: return 0
+        i = 0
+        res = len(s)
+        for j in range(len(s)):
+            if s[j] in extras:
+                extras[s[j]] -= 1
+            
+            while max(extras.values()) <= 0:
+                res = min(res, j-i+1)
+                if s[i] in extras:
+                    extras[s[i]] += 1
+                i += 1
+                
+                
+        return res
