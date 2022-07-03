aa25836162478849e6cec20799e8f446aa5b63b2
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-04 02:22:06 +0530

    Time: 68 ms (5.34%), Space: 13.9 MB (60.97%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -4,12 +4,12 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         l,r = 1,n
-        res = sys.maxsize
+        # res = sys.maxsize
         while l<=r:
             m = (l+r)//2
             if not isBadVersion(m):
                 l = m+1
             else:
-                res = min(m,res)
+                # res = min(m,res)
                 r = m-1
-        return res
+        return l
