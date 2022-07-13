7416f2de3473733ae680bf476fe89565e6abef9f
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-13 15:05:23 +0530

    Time: 56 ms (69.13%), Space: 14.2 MB (44.39%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,11 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(set(s))!= len(set(t)):
+        if len(set(s)) != len(set(t)):
             return False
         l = {}
-        for i,j in zip(s, t):
+        for i,j in zip(s,t):
             if i not in l:
                 l[i] = j
-            if l.get(i) != j:
-                return False
+            else:
+                if l[i] != j:
+                    return False
         return True
