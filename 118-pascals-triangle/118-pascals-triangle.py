da957b6fb38ac149c8889710cb5bf8af7bd04651
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-15 01:25:51 +0530

    Time: 37 ms (81.15%), Space: 13.8 MB (97.62%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,15 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        p = 1
-        f = [p]
-        for i in range(1, numRows+1):
-            p *= i
-            f.append(p)
-        
-        res = []
-        for i in range(numRows):
-            a = []
-            for j in range(i+1):
-                a.append(f[i]//(f[j]*f[i-j]))
-            res.append(a)
-        return res
+        if numRows == 1:
+            return [[1]]
+        ans = [[1],[1,1]]
+        for i in range(2,numRows):
+            x = [1]
+            for k in range(1,i):
+                x.append(ans[-1][k-1]+ans[-1][k])
+            x.append(1)
+            ans.append(x)
+        return ans
