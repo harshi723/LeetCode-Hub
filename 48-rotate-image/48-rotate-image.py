6b9a0cd520dc04694d0157a38cafb5be0a6fd90c
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-20 04:18:01 +0530

    Time: 59 ms (39.50%), Space: 13.9 MB (30.03%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -3,20 +3,8 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        t, b, n = 0, len(matrix)-1, len(matrix)        
-        while t<b:
-            x = []
-
-            for i in range(n):
-                x.append(matrix[t][t+i])
-            for i in range(n):
-                matrix[t][b-i] = matrix[t+i][t]
-            for i in range(n):
-                matrix[t+i][t] = matrix[b][t+i]
-            for i in range(n):
-                matrix[b][t+i] = matrix[b-i][b]
-            for i in range(n):
-                matrix[t+i][b] = x[i]
-            t+=1
-            b-=1
-            n -=2
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(len(matrix)):
+            matrix[i].reverse()
