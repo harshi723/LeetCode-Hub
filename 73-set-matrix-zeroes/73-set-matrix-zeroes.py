27181803573cf9347e71945251a091d121a88b95
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-19 15:10:54 +0530

    Time: 240 ms (24.43%), Space: 14.7 MB (52.85%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,17 +3,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        def zeroIt(matrix, i, j):
+            for m in range(len(matrix)):
+                if matrix[m][j] != 'X':
+                    matrix[m][j] = 0
+            for m in range(len(matrix[0])):
+                if matrix[i][m] != 'X':
+                    matrix[i][m] = 0
+        
         x = []
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    x.append((i,j))
+                    matrix[i][j] = 'X'
         # print(x)
-        for num in x:
-            r,c = num[0],num[1]
-            for i in range(len(matrix)):
-                matrix[i][c] = 0
-            for i in range(len(matrix[0])):
-                matrix[r][i] = 0
-                    
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 'X':
+                    zeroIt(matrix, i, j)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 'X':
+                    matrix[i][j]= 0
+                
                 
