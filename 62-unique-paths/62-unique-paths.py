d759418f9d9283ad293ccba1ee683591e18b866d
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-20 17:52:33 +0530

    Time: 47 ms (55.38%), Space: 13.9 MB (32.87%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -6,12 +6,19 @@ class Solution:
 #             else: return func(i,j+1,m,n) + func(i+1,j,m,n)
 #         return func(0,0,m,n)
     
-        dp = [[-1 for i in range(n)] for j in range(m)]
-        def func(i,j,m,n,dp):
-            if i==m-1 and j==n-1: return 1
-            elif i<0 or i==m or j<0 or j==n: return 0
-            elif dp[i][j]!=-1: return dp[i][j]
-            else: 
-                dp[i][j] = func(i,j+1,m,n,dp) + func(i+1,j,m,n,dp)
-                return dp[i][j]
-        return func(0,0,m,n,dp)
+        # dp = [[-1 for i in range(n)] for j in range(m)]
+        # def func(i,j,m,n,dp):
+        #     if i==m-1 and j==n-1: return 1
+        #     elif i<0 or i==m or j<0 or j==n: return 0
+        #     elif dp[i][j]!=-1: return dp[i][j]
+        #     else: 
+        #         dp[i][j] = func(i,j+1,m,n,dp) + func(i+1,j,m,n,dp)
+        #         return dp[i][j]
+        # return func(0,0,m,n,dp)
+        
+        N = n+m-2
+        r = m-1
+        res = 1
+        for i in range(1,r+1):
+            res = res*(N-r+i)/i
+        return int(res)
