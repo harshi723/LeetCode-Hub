2b03d359f3960d908bbd5398a882bafbf005b1a5
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-07-21 17:17:23 +0530

    Time: 118 ms (35.27%), Space: 13.8 MB (86.51%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -3,24 +3,22 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        c = 0
-        p = None
-        while l1 is not None or l2 is not None:
-            x = l1.val if l1 is not None else 0
-            y = l2.val if l2 is not None else 0
-            res = x+y+c
-            c = res//10
-            temp = ListNode(res%10)
-            if p == None:
-                p = temp
-                curr = temp
-            else:
-                curr.next = temp
-                curr = curr.next
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
-        if c: curr.next = ListNode(c)
-        return p
+        list3 = ListNode()
+        temp = list3
+        carry = 0
+        while l1 or l2 or carry:
+            s = 0
+            if l1:
+                s += l1.val
+                l1 = l1.next
+            if l2: 
+                s += l2.val
+                l2 = l2.next
+            s += carry
+            carry = s//10
+            node = ListNode(s%10)
+            temp.next = node
+            temp = temp.next
+        return list3.next
