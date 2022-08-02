f82a391828c69a5e3997ac95ca659b014aae9099
Harshita Ag <72163561+harshi723@users.noreply.github.com>
2022-08-03 03:35:29 +0530

    Time: 61 ms (45.97%), Space: 13.9 MB (30.80%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -5,17 +5,33 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head or not head.next: return head
-        x = []
-        curr = head
-        while curr:
-            x.append(curr.val)
-            curr = curr.next
-        if k>=len(x): k %= len(x)
-        if k==0: return head
-        x[:] = x[len(x)-k:] + x[:len(x)-k]
-        curr = head
-        for i in x:
-            curr.val = i
-            curr = curr.next
+        if not head or not head.next or k==0: return head
+        # x = []
+        # curr = head
+        # while curr:
+        #     x.append(curr.val)
+        #     curr = curr.next
+        # if k>=len(x): k %= len(x)
+        # if k==0: return head
+        # x[:] = x[len(x)-k:] + x[:len(x)-k]
+        # curr = head
+        # for i in x:
+        #     curr.val = i
+        #     curr = curr.next
+        # return head
+        
+        temp = head
+        l = 1
+        while temp.next:
+            temp = temp.next
+            l += 1
+        temp.next = head
+        k %= l
+        c = l-k
+        temp = head
+        for i in range(c-1):
+            temp = temp.next
+        head = temp.next
+        temp.next = None
         return head
+        
